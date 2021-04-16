5eeb72547b789777434dffbe300c8b7b9b5e3ddf
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-04-16 10:40:54 -0700

    add props

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,41 @@ import logo from "./logo.svg";
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+export default function App() {
+  const expenses = [
+    { date: new Date(2021, 3, 16), title: "Car Insurance", amount: 405.72 },
+    { date: new Date(2021, 3, 15), title: "Phone bill", amount: 80.02 },
+    { date: new Date(2021, 3, 15), title: "Rent", amount: 900.0 },
+    { date: new Date(2021, 3, 14), title: "Groceries", amount: 152.13 },
+    { date: new Date(2021, 3, 13), title: "Liquor", amount: 63.32 },
+  ];
   return (
     <div>
-      <h2>Let's Get Started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      />
+      <ExpenseItem
+        date={expenses[4].date}
+        title={expenses[4].title}
+        amount={expenses[4].amount}
+      />
     </div>
   );
 }
-
-export default App;
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>April 13 2020</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$405.89</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
 }
-
-export default ExpenseItem;
