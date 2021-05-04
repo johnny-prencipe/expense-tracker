a9bfdaa3ba752eaa44a9c05f9f8a951a5d7b2289
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-05-03 23:41:43 -0700

    add event listener vid 46

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -3,6 +3,9 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 export default function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
@@ -10,6 +13,7 @@ export default function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
