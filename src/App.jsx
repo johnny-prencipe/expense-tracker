d4c95d3fca5a9418de6fb940b7fd65c73e6a2348
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-05-16 21:29:55 -0700

    add IDs to expenses

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Expenses from "./components/Expenses/Expenses";
 
 export default function App() {
   const expenses = [
-    { date: new Date(2021, 3, 16), title: "Car Insurance", amount: 405.72 },
-    { date: new Date(2021, 3, 15), title: "Phone bill", amount: 80.02 },
-    { date: new Date(2021, 3, 15), title: "Rent", amount: 450.0 },
-    { date: new Date(2021, 3, 14), title: "Groceries", amount: 152.13 },
-    { date: new Date(2021, 3, 13), title: "Liquor", amount: 63.32 },
-    { date: new Date(2021, 2, 31), title: "Rent", amount: 450.0 },
+    { id: 'e1', date: new Date(2021, 3, 16), title: "Car Insurance", amount: 405.72 },
+    { id: 'e2', date: new Date(2021, 3, 15), title: "Phone bill", amount: 80.02 },
+    { id: 'e3', date: new Date(2021, 3, 15), title: "Rent", amount: 450.0 },
+    { id: 'e4', date: new Date(2021, 3, 14), title: "Groceries", amount: 152.13 },
+    { id: 'e5', date: new Date(2021, 3, 13), title: "Liquor", amount: 63.32 },
+    { id: 'e6', date: new Date(2021, 2, 31), title: "Rent", amount: 450.0 },
   ];
 
   return (
