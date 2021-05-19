eb2c59dabbe4246cac53d29f5d3b9f1f4390d6b0
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-05-18 17:03:13 -0700

    prettier lines

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -4,8 +4,8 @@ import "./ExpenseDate.css";
 
 export default function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-us", { month: "long" });
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  const day   = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year  = props.date.getFullYear();
 
   return (
     <div className="expense-date">
