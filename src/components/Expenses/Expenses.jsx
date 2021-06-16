6094b3e59feeeec2b5201c16990781eeb41349ef
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-06-15 23:43:41 -0700

    Year filter working.

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -8,7 +8,12 @@ import Card from '../UI/Card';
 export default function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
 
-  const expenses = props.expenses.map(expense => {
+  const filteredExpenses = props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  const expenses = filteredExpenses.map(expense => {
+    console.log(filteredExpenses);
     return (
       <ExpenseItem
         key={expense.id}
