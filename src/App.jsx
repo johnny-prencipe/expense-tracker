0711f347ff4a431d4d13b2bd959ed330aa923889
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-06-15 23:11:36 -0700

    Can now add new expenses! Video 64

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
+  { id: 'e1', date: new Date(2021, 3, 16), title: 'Car Insurance', amount: 405.72 },
+  { id: 'e2', date: new Date(2021, 3, 15), title: 'Phone bill', amount: 80.02 },
+  { id: 'e3', date: new Date(2021, 3, 15), title: 'Rent', amount: 450.0 },
+  { id: 'e4', date: new Date(2021, 3, 14), title: 'Groceries', amount: 152.13 },
+  { id: 'e5', date: new Date(2021, 3, 13), title: 'Liquor', amount: 63.32 },
+  { id: 'e6', date: new Date(2021, 2, 31), title: 'Rent', amount: 450.0 },
+];
 
 export default function App() {
-  const expenses = [
-    { id: 'e1', date: new Date(2021, 3, 16), title: "Car Insurance", amount: 405.72 },
-    { id: 'e2', date: new Date(2021, 3, 15), title: "Phone bill", amount: 80.02 },
-    { id: 'e3', date: new Date(2021, 3, 15), title: "Rent", amount: 450.0 },
-    { id: 'e4', date: new Date(2021, 3, 14), title: "Groceries", amount: 152.13 },
-    { id: 'e5', date: new Date(2021, 3, 13), title: "Liquor", amount: 63.32 },
-    { id: 'e6', date: new Date(2021, 2, 31), title: "Rent", amount: 450.0 },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('addExpenseHandler fired from App.jsx');
-    console.log(expense);
-  }
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
