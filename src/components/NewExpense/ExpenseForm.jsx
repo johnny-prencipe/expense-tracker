73de46dec62d2e54cd374aa57cbbae1fe98c0da9
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-06-16 20:14:26 -0700

    Allow to hide or show expense form. Needs refactoring

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -3,15 +3,9 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 export default function ExpenseForm(props) {
-  // State handlers for input form
-  const [enteredTitle, setEnteredTitle]   = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate]     = useState('');
-
-  const titleChangeHandler  = event => setEnteredTitle(event.target.value);
+  const titleChangeHandler = event => setEnteredTitle(event.target.value);
   const amountChangeHandler = event => setEnteredAmount(event.target.value);
-  const dateChangeHandler   = event => setEnteredDate(event.target.value);
-
+  const dateChangeHandler = event => setEnteredDate(event.target.value);
   const submitHandler = event => {
     event.preventDefault();
 
@@ -31,41 +25,63 @@ export default function ExpenseForm(props) {
     }
   };
 
-  return (
-    <form autoComplete="off" onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+  const hideFormHandler = event => {
+    setForm(hiddenForm);
+  };
+  const showFormHandler = event => {
+    setForm(
+      <form autoComplete="off" onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2023-12-31"
+              value={enteredDate}
+              onChange={dateChangeHandler}
+            />
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
+        <div className="new-expense__actions">
+          <button type="button" onClick={hideFormHandler}>
+            Cancel
+          </button>
+          <button type="submit">Add Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2023-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+      </form>
+    );
+  };
+  const hiddenForm = (
+    <div>
+      <button type="submit" onClick={showFormHandler}>
+        Show Form
+      </button>
+    </div>
   );
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  const [form, setForm] = useState(hiddenForm);
+
+  return form;
 }
